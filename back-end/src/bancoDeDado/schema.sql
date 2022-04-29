26aa9288ad83a5e88cabe3dcf3205184167c38e5
Henrique Almeida <hart.aldtore@gmail.com>
2022-04-29 19:53:52 -0300

    conexao com BD

--- back-end/src/bancoDeDado/schema.sql
+++ back-end/src/bancoDeDado/schema.sql
@@ -5,7 +5,6 @@ drop table if exists usuarios;
 create table usuarios (
 	id serial primary key,
   	nome varchar(255) not null,
-  	cpf varchar(15) unique not null,
     email text unique not null,
     endereco text not null,
     carteira integer not null default 0
